1b572e075725893ce3ebce3ed795d0731ccdc1db
BARIŞ COŞKUN <162376300+Bariscompeng@users.noreply.github.com>
2025-11-21 21:02:38 +0300

    Update line_follower_node.py

--- ros2_ws/src/line_follower/line_follower/line_follower_node.py
+++ ros2_ws/src/line_follower/line_follower/line_follower_node.py
@@ -7,7 +7,7 @@ class LineFollowerNode(Node):
     def __init__(self):
         super().__init__('line_follower_node')
 
-        # Hata (line_error) subscriber
+        
         self.error_sub = self.create_subscription(
             Float32,
             'line_error',
@@ -15,54 +15,52 @@ class LineFollowerNode(Node):
             10
         )
 
-        # Drone kontrol topic'i
+        
         self.cmd_pub = self.create_publisher(
             Twist,
-            '/model/M100/cmd_vel',   # topiğin doğru olduğundan eminsin zaten
+            '/model/M100/cmd_vel',   
             10
         )
 
-        # Kontrol kazancı
+        
         self.Kp = 0.8
 
-        # Sabit ileri hız
+       
         self.forward_speed = 0.3
 
-        # Son hata ve son hata zamanı
+        
         self.current_error = 0.0
         self.last_error_time = self.get_clock().now()
 
-        # Kaç saniye boyunca line_error gelmezse duralım?
-        self.lost_line_timeout = 0.5  # 0.5 saniye
+        
+        self.lost_line_timeout = 0.5  
 
-        # Belirli aralıklarla control_loop çalışsın
-        self.control_timer = self.create_timer(0.05, self.control_loop)  # 20 Hz
+        
+        self.control_timer = self.create_timer(0.05, self.control_loop)  
 
         self.get_logger().info("Line follower node started with timeout logic.")
 
     def error_callback(self, msg: Float32):
-        # Her yeni hata geldiğinde kaydet ve zamanı güncelle
+        
         self.current_error = msg.data
         self.last_error_time = self.get_clock().now()
 
     def control_loop(self):
-        # Burada periyodik olarak cmd_vel basıyoruz
+        
         now = self.get_clock().now()
-        dt = (now - self.last_error_time).nanoseconds / 1e9  # saniye cinsinden
+        dt = (now - self.last_error_time).nanoseconds / 1e9  
 
         cmd = Twist()
 
         if dt < self.lost_line_timeout:
-            # Çizgi yakın zamanda görüldü → normal takip
+           
             cmd.linear.x = self.forward_speed
             cmd.angular.z = self.Kp * self.current_error
         else:
-            # Uzun süredir line_error gelmiyor → dur
+            
             cmd.linear.x = 0.0
             cmd.angular.z = 0.0
-            # (İstersen burada ufak bir arama davranışı da ekleyebilirsin:
-            # cmd.angular.z = 0.3 vs. diyerek olduğu yerde dönsün.)
-
+            
         self.cmd_pub.publish(cmd)
 
 
